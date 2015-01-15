33928323357cd5b80a42614fc22e0d604f869bc2
Daniel Manesajian <manesajian@gmail.com>
2015-01-15 15:40:24 -0800

    Removed debugging.

--- superq.py
+++ superq.py
@@ -2320,11 +2320,6 @@ class SuperQStreamHandler(StreamRequestHandler):
         response = SuperQNodeResponse()
         response.msg_id = request.msg_id
 
-        # useful for debugging
-        self.log('Msg: cmd= {0}, args= {1}, body= {2}'.format(request.cmd,
-                                                              request.args,
-                                                              request.body))
-
         cmd = request.cmd
         args = request.args
         body = request.body
