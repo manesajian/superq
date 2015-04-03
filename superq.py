82a0b4eada4bafb34c9b3d31f1954412c4046f70
Live session user <manesajian@gmail.com>
2015-04-02 17:23:21 -0700

    Removed unused DBTypeError exception.

--- superq.py
+++ superq.py
@@ -399,12 +399,6 @@ class DBExecError(SuperQEx):
     def __str__(self):
         return repr(self.value)
 
-class DBTypeError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-
 class MalformedNetworkRequest(Exception):
     def __init__(self, value):
         self.value = value
