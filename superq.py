5fe00e4c6dae93b8af566dcc6b0fbd365966c221
Live session user <manesajian@gmail.com>
2015-04-04 00:16:00 -0700

    Removed redundant name attr from superq file save function.

--- superq.py
+++ superq.py
@@ -1596,7 +1596,6 @@ class superq():
 
             # serialize relevant attributes as name-value pairs
             sqAttrs = ''
-            sqAttrs += 'name|{0},'.format(self.name)
             sqAttrs += 'host|{0},'.format(self.host)
             sqAttrs += 'keyCol|{0},'.format(self.keyCol)
             sqAttrs += 'maxlen|{0},'.format(self.maxlen)
